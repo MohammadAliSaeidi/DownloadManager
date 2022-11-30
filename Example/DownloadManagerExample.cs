9dda64a809c45ff3d64aa765ed34703402e8f4ed
MohamadAli Saeidi <1saeidima@gmail.com>
2022-12-01 00:59:35 +0330

    modified:   Example/DownloadManagerExample.cs modified:   Example/DownloadManagerExample.unity

--- Example/DownloadManagerExample.cs
+++ Example/DownloadManagerExample.cs
@@ -1,27 +1,50 @@
-using UnityEngine;
 using Services.DownloadService;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
 
-	public class DownloadManagerExample : MonoBehaviour
+public class DownloadManagerExample : MonoBehaviour
+{
+	[SerializeField]
+	private Button btn_Download;
+
+	[SerializeField]
+	private InputField inp_FileURL;
+
+	[SerializeField]
+	private Slider slider_DownloadPercentage;
+
+	private void Start()
 	{
-		[ContextMenu("Download Example File")]
-		public void DownloadExampleFile()
+		btn_Download.onClick.AddListener(() => DownloadImage());
+	}
+
+	private void DownloadImage()
+	{
+		if (inp_FileURL.text.Length > 0)
 		{
 			var downloadRequest = new DownloadRequest()
 			{
 				downloadMode = DownloadMode.Sequential,
 				downloadPriority = DownloadPriority.Normal,
-				FileWebURLAddress = "https://upload.wikimedia.org/wikipedia/en/thumb/8/80/Wikipedia-logo-v2.svg/1200px-Wikipedia-logo-v2.svg.png",
+				FileWebURLAddress = inp_FileURL.text,
 				SavePath = Application.dataPath
 			};
+
 			var downloadProgress = DownloadManager.QueueDownloadRequest(downloadRequest);
 
-			System.Threading.Tasks.Task.Run(() =>
-			{
-				while (downloadProgress.IsDownloading)
-				{
-					Debug.Log(downloadProgress.GetDownloadingState().ProgressPercentage);
-					System.Threading.Thread.Sleep(500);
-				}
-			});
+			StartCoroutine(Co_UpdateUI(downloadProgress));
 		}
+
 	}
+	private IEnumerator Co_UpdateUI(DownloadProgress downloadProgress)
+	{
+		while (downloadProgress.IsDownloading)
+		{
+			slider_DownloadPercentage.value = downloadProgress.GetDownloadingState().ProgressPercentage / 100.0f;
+			yield return new WaitForSeconds(0.1f);
+			yield return null;
+		}
+		slider_DownloadPercentage.value = 1;
+	}
+}
